39244c218c3ac3d2f11911155a4d7a2a4c93997e
Juan Carrero <juancarrero811@gmail.com>
2020-03-02 11:39:54 -0400

    Delete macros.h

--- macros.h
+++
@@ -1,9 +0,0 @@
-#ifndef MACROSFETCH_H
-#define MACROSFETCH_H
-
-#define NUM_INSTRUCTIONS 8
-#define INSTRUCTION_SIZE 16
-#define ARCHIVE "instructionMen.txt"
-#define TAM_PIPE 4
-
-#endif // MACROSFETCH_H
